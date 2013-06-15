8a30759a81ab74202193e54f01d15d358f32f197
Anoop Hallur <anooprh@thoughtworks.com>
2013-06-15 17:57:05 +0530

    will not download if file already exists

--- downloadAll.sh
+++ downloadAll.sh
@@ -4,7 +4,7 @@ SVG_DIR_NAME=svg
 mkdir  $SVG_DIR_NAME
 countries=`cat $COUNTRIES_LIST_FILE_NAME`
 for country in $countries; do
-    if [ -f /svg/$country.svg ]; then 
+    if [ -e svg/$country.svg ]; then 
         echo "File : $country.svg already exists"
     else    
         wget "https://en.wikipedia.org/wiki/File:$country%5F(orthographic_projection).svg" -O svg/$country.svg
