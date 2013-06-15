52ed3c9ca91684c66c68dd1168750d854b938e46
Anoop Hallur <anooprh@thoughtworks.com>
2013-06-15 17:53:29 +0530

    need to add already existing check

--- downloadAll.sh
+++ downloadAll.sh
@@ -1,9 +1,13 @@
 COUNTRIES_LIST_FILE_NAME=countries-list
+SVG_DIR_NAME=svg
 
-mkdir svg
+mkdir  $SVG_DIR_NAME
 countries=`cat $COUNTRIES_LIST_FILE_NAME`
 for country in $countries; do
-    echo $country
-    wget -q "https://en.wikipedia.org/wiki/File:$country%5F(orthographic_projection).svg" -O svg/$country.svg
+    if [ -f /svg/$country.svg ]; then 
+        echo "File : $country.svg already exists"
+    else    
+        wget "https://en.wikipedia.org/wiki/File:$country%5F(orthographic_projection).svg" -O svg/$country.svg
+    fi
 done
 
